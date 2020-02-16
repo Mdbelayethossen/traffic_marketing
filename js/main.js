e83cd2834a2d53ecd89b0d27873506547f065e7f
Mdbelayethossen <59694089+Mdbelayethossen@users.noreply.github.com>
2020-02-16 10:15:49 +0600

    update

--- js/main.js
+++ js/main.js
@@ -41,6 +41,42 @@ $(document).ready(function () {
             }
         }
     })
+    // Hero Area Owl
+	$(' .hero_area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        dots: false,
+        nav: false,
+        autoplayHoverPause:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            320: {
+                items: 1
+            },
+            560: {
+                items: 1
+            },
+            1200: {
+                items: 1
+            }
+        }
+    })
+// Custom button
+    var owl = $('.slider');
+    owl.owlCarousel();
+    // Go to the next item
+    $('.owl-next').click(function() {
+        owl.trigger('next.owl.carousel');
+    })
+    // Go to the previous item
+    $('.owl-prev').click(function() {
+        // With optional speed parameter
+        // Parameters has to be in square bracket '[]'
+        owl.trigger('prev.owl.carousel', [300]);
+    })
+
 
 
 })
